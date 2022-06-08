462d1c12b6de676b17d163414f6863c2d03f8412
ratikkoka <koka.ratik@yahoo.com>
2022-06-08 13:35:44 -0700

    Formatted Code Style

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -3,13 +3,11 @@ import TrendGraph from "./TrendGraph";
 import { usePapaParse } from "react-papaparse";
 
 export default function Stats() {
-
   let [deaths, setDeaths] = useState([]);
   let [injury, setInjury] = useState([]);
   let [mass, setMass] = useState([]);
 
   const { readString } = usePapaParse();
-
 
   let datasets = [
     "/data/total-deaths-geoencoded.csv",
@@ -20,80 +18,67 @@ export default function Stats() {
   useEffect(() => {
     for (let i = 0; i < datasets.length; i++) {
       fetch(process.env.PUBLIC_URL + datasets[i])
-      .then((response) => response.text())
-      .then((responseText) => {
-        readString(responseText, {
-          worker: true,
-          header: true,
-          complete: (results) => {
-            if (i == 0) {
-              setDeaths(results.data);
-            } else if (i == 1) {
-              setInjury(results.data);
-            } else {
-              setMass(results.data);
-            }
-          },
+        .then((response) => response.text())
+        .then((responseText) => {
+          readString(responseText, {
+            worker: true,
+            header: true,
+            complete: (results) => {
+              if (i == 0) {
+                setDeaths(results.data);
+              } else if (i == 1) {
+                setInjury(results.data);
+              } else {
+                setMass(results.data);
+              }
+            },
+          });
         });
-      });
     }
-    
   });
 
   return (
     <div className="dashboard">
-            <div className={"dashboard-container"}>
-                <section>
-                    <h1 className="data-head">Shootings</h1>
-                    <h2 className="data-head">
-                        as of <span>6/6/2022</span>.
-                    </h2>
-                    <div className="data-cd">
-                        <div>
-                            <h3>Deaths</h3>
-                            <p>
-                              18,961
-                            </p>
-                        </div>
-                        <div>
-                            <h3>Injuries</h3>
-                            <p>
-                              16,005
-                            </p>
-                        </div>
-                    </div>
-                </section>
+      <div className={"dashboard-container"}>
+        <section>
+          <h1 className="data-head">Shootings</h1>
+          <h2 className="data-head">
+            as of <span>6/6/2022</span>.
+          </h2>
+          <div className="data-cd">
+            <div>
+              <h3>Deaths</h3>
+              <p>18,961</p>
             </div>
-            <div className="widgets-container">
-                <div className="widget">
-                    <h2>Total Deaths Trend</h2>
-                    <TrendGraph 
-                      data={deaths} 
-                      type="# Killed" 
-                      title="Deaths" />
-                </div>
-                <div className="widget">
-                    <h2>Total Injuries Trend</h2>
-                    <TrendGraph
-                        data={injury}
-                        type="# Injured"
-                        title="Injuries" />
-                </div>
-                <div className="widget">
-                    <h2>Mass Shootings Trend</h2>
-                    <TrendGraph 
-                      data={mass} 
-                      type="# Killed" 
-                      title="Mass Shooting Deaths" />
-                </div>
-                <div className="widget">
-                    <h2>Shooting Rates</h2>
-                    <TrendGraph
-                        data={deaths}
-                        type="rateKill"
-                        title="Shooting Rates" />
+            <div>
+              <h3>Injuries</h3>
+              <p>16,005</p>
             </div>
-            </div>
+          </div>
+        </section>
+      </div>
+      <div className="widgets-container">
+        <div className="widget">
+          <h2>Total Deaths Trend</h2>
+          <TrendGraph data={deaths} type="# Killed" title="Deaths" />
         </div>
+        <div className="widget">
+          <h2>Total Injuries Trend</h2>
+          <TrendGraph data={injury} type="# Injured" title="Injuries" />
+        </div>
+        <div className="widget">
+          <h2>Mass Shootings Trend</h2>
+          <TrendGraph
+            data={mass}
+            type="# Killed"
+            title="Mass Shooting Deaths"
+          />
+        </div>
+        <div className="widget">
+          <h2>Shooting Rates</h2>
+          <TrendGraph data={deaths} type="rateKill" title="Shooting Rates" />
+        </div>
+      </div>
+    </div>
   );
 }
--- src/components/TrendGraph.js
+++ src/components/TrendGraph.js
@@ -3,102 +3,100 @@ import { Line } from "react-chartjs-2";
 import "chart.js/auto";
 import "chartjs-adapter-date-fns";
 
-export default function CasesGraph({data, type, title}) {
+export default function CasesGraph({ data, type, title }) {
+  var parsedData = [];
+  let scale = { text: "date", type: "time" };
+  if (type != "rateKill") {
+    var holder = {};
 
-    var parsedData = [];
-    let scale = {text: 'date', type: 'time'};
-    if (type != 'rateKill') {
-        var holder = {};
+    data.forEach(function (day) {
+      if (holder.hasOwnProperty(day["Incident Date"])) {
+        holder[day["Incident Date"]] =
+          holder[day["Incident Date"]] + parseInt(day[type]);
+      } else {
+        holder[day["Incident Date"]] = parseInt(day[type]);
+      }
+    });
 
-        data.forEach(function(day) {
-        if (holder.hasOwnProperty(day["Incident Date"])) {
-            holder[day["Incident Date"]] = holder[day["Incident Date"]] + parseInt(day[type]);
-        } else {
-            holder[day["Incident Date"]] = parseInt(day[type]);
-        }
-        });
-
-        for (var prop in holder) {
-            parsedData.push({ x: new Date(prop), y: holder[prop] });
-        }
-    } else {
-        var holder = {};
-
-        data.forEach(function(day) {
-            let float = isNaN(parseFloat(day[type])) ? 0 : parseFloat(day[type]);
-            if (holder.hasOwnProperty(day["State"])) {
-                holder[day["State"]] = holder[day["State"]] + float;
-            } else {
-                holder[day["State"]] = float;
-            }
-        });
-
-        
-
-        for (var prop in holder) {
-            parsedData.push({ x: prop, y: holder[prop] });
-        }
-        scale = {text: 'state'};
+    for (var prop in holder) {
+      parsedData.push({ x: new Date(prop), y: holder[prop] });
     }
+  } else {
+    var holder = {};
 
-    return (
-        <Line
-            data={{
-                datasets: [
-                    {
-                        data: parsedData,
-                        borderColor: "rgba(255,0,0,0.8)",
-                        borderWidth: 1,
-                        fill: {
-                            target: "origin",
-                            above: "rgba(255,0,0,0.3)",
-                            below: "rgba(255,0,0,0.3)",
-                        },
-                    },
-                ],
-            }}
-            options={{
-                hover: {
-                    mode: "nearest",
-                    axis: "x",
-                    intersect: false,
-                },
-                elements: {
-                    point: {
-                        radius: 0,
-                        hoverRadius: 5,
-                        backgroundColor: "white",
-                    },
-                },
-                scales: {
-                    x: scale,
-                    y: {
-                        text: `${title}`,
-                    },
-                },
-                plugins: {
-                    legend: {
-                        display: false,
-                    },
-                    tooltip: {
-                        mode: "nearest",
-                        axis: "x",
-                        intersect: false,
-                        callbacks: {
-                            title: context => {
-                                if (type == 'rateKill') {
-                                    return context[0].raw.x;
-                                }
-                                return context[0].raw.x.toLocaleDateString();
-                            },
-                            beforeLabel: function () {
-                                let label = `${title}:`;
-                                return label;
-                            },
-                        },
-                    },
-                },
-            }}
-        />
-    );
+    data.forEach(function (day) {
+      let float = isNaN(parseFloat(day[type])) ? 0 : parseFloat(day[type]);
+      if (holder.hasOwnProperty(day["State"])) {
+        holder[day["State"]] = holder[day["State"]] + float;
+      } else {
+        holder[day["State"]] = float;
+      }
+    });
+
+    for (var prop in holder) {
+      parsedData.push({ x: prop, y: holder[prop] });
+    }
+    scale = { text: "state" };
+  }
+
+  return (
+    <Line
+      data={{
+        datasets: [
+          {
+            data: parsedData,
+            borderColor: "rgba(255,0,0,0.8)",
+            borderWidth: 1,
+            fill: {
+              target: "origin",
+              above: "rgba(255,0,0,0.3)",
+              below: "rgba(255,0,0,0.3)",
+            },
+          },
+        ],
+      }}
+      options={{
+        hover: {
+          mode: "nearest",
+          axis: "x",
+          intersect: false,
+        },
+        elements: {
+          point: {
+            radius: 0,
+            hoverRadius: 5,
+            backgroundColor: "white",
+          },
+        },
+        scales: {
+          x: scale,
+          y: {
+            text: `${title}`,
+          },
+        },
+        plugins: {
+          legend: {
+            display: false,
+          },
+          tooltip: {
+            mode: "nearest",
+            axis: "x",
+            intersect: false,
+            callbacks: {
+              title: (context) => {
+                if (type == "rateKill") {
+                  return context[0].raw.x;
+                }
+                return context[0].raw.x.toLocaleDateString();
+              },
+              beforeLabel: function () {
+                let label = `${title}:`;
+                return label;
+              },
+            },
+          },
+        },
+      }}
+    />
+  );
 }
